a2d25c43b9ff66b230333ab2a04dea6685e7841a
max.b <max.b@dev.izibook.ru>
2024-11-07 00:28:13 +0300

    fix

--- index.js
+++ index.js
@@ -2,48 +2,43 @@ import path from 'path';
 import {fileURLToPath} from 'url';
 import express from 'express';
 import {nanoid} from 'nanoid';
-
-// подключим multer
 import multer from 'multer';
 
 const app = express();
 
-// инициализируем хранилище
 const diskStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
+  destination: (_req, file, cb) => {
     const imageExtensions = ['.jpg', '.png', '.svg'];
     const extension = path.extname(file.originalname);
-    // Важно! Директории должны существовать
     const uploadPath = (imageExtensions.includes(extension)) ? 'upload/images' : 'upload';
     cb(null, uploadPath);
   },
-  filename: (req, file, cb) => {
-    // Подготовим уникальное имя для файла
+  filename: (_req, file, cb) => {
     const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1E9);
     const extension = path.extname(file.originalname);
     cb(null, `${uniqueName}${extension}`);
   },
 
-  
 });
-
-
-
-// инициализируем middleware
+function errHandler(err, req,res,next){
+  if(err.code) 
+    res.send(err.code);
+  else
+    next()
+}
 const upload = multer({storage: diskStorage, limits: {
-  files: 2,
-  fileSize: 1024 * 1024,
-},});
+    files: 2,
+    fileSize: 1024 * 1024,
+  }
+});
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static(path.join(__dirname, 'static')));
 app.use(express.static(path.join(__dirname, 'upload/images')));
 
-// добавим middleware в цепочку обработчиков
-app.post('/', upload.array('files', 5), (req, res) => {
+app.post('/', upload.array('files', 5), errHandler,  (req, res) => {
   console.log(req.files);
-  res.send('OK');
+  res.send('OK фаил успешно загружен');
 });
-
 app.listen(8080);
